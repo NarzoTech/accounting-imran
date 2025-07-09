63f4e3b07807a4bf72d6e4a117974da9e6792833
progRizvi <prog.rizvi@gmail.com>
2025-07-09 20:52:18 +0600

    routes file added

---
+++ Modules/Accounting/resources/views/sidebar.blade.php
@@ -0,0 +1,108 @@
+<li class="menu-item {{ isRoute(['admin.invoice.*', 'admin.customer.*', 'admin.container.*'], 'active open') }}">
+
+
+    <a href="javascript:void(0);" class="menu-link menu-toggle">
+        <i class='menu-icon tf-icons bx bx-news'></i>
+        <div class="text-truncate" data-i18n="{{ __('Sales & Payment') }}">{{ __('Sales & Payment') }}</div>
+    </a>
+
+    <ul class="menu-sub">
+        <li class="menu-item {{ isRoute('admin.invoice.*', 'active') }}">
+            <a class="menu-link" href="{{ route('admin.invoice.index') }}">
+                {{ __('Invoice') }}
+            </a>
+        </li>
+        <li class="menu-item {{ isRoute('admin.customer.*', 'active') }}">
+            <a class="menu-link" href="{{ route('admin.customer.index') }}">
+                {{ __('Customer') }}
+            </a>
+        </li>
+        <li class="menu-item {{ isRoute('admin.container.*', 'active') }}">
+            <a class="menu-link" href="{{ route('admin.container.index') }}">
+                {{ __('Container') }}
+            </a>
+        </li>
+    </ul>
+</li>
+
+
+<li class="menu-item {{ isRoute(['admin.product.*', 'admin.category.*'], 'active open') }}">
+
+
+    <a href="javascript:void(0);" class="menu-link menu-toggle">
+        <i class='menu-icon tf-icons bx bx-news'></i>
+        <div class="text-truncate" data-i18n="{{ __('Product & Service') }}">{{ __('Product & Service') }}</div>
+    </a>
+
+    <ul class="menu-sub">
+        <li class="menu-item {{ isRoute('admin.product.*', 'active') }}">
+            <a class="menu-link" href="{{ route('admin.product.index') }}">
+                {{ __('Product') }}
+            </a>
+        </li>
+        <li class="menu-item {{ isRoute('admin.category.*', 'active') }}">
+            <a class="menu-link" href="{{ route('admin.category.index') }}">
+                {{ __('Category') }}
+            </a>
+        </li>
+    </ul>
+</li>
+
+<li
+    class="menu-item {{ isRoute(['admin.income.*', 'admin.transfer.*', 'admin.expense.*', 'admin.investor.*'], 'active open') }}">
+
+
+    <a href="javascript:void(0);" class="menu-link menu-toggle">
+        <i class='menu-icon tf-icons bx bx-news'></i>
+        <div class="text-truncate" data-i18n="{{ __('Accounting') }}">{{ __('Accounting') }}</div>
+    </a>
+
+    <ul class="menu-sub">
+        <li class="menu-item {{ isRoute('admin.income.*', 'active') }}">
+            <a class="menu-link" href="{{ route('admin.income.index') }}">
+                {{ __('Income') }}
+            </a>
+        </li>
+        <li class="menu-item {{ isRoute('admin.transfer.*', 'active') }}">
+            <a class="menu-link" href="{{ route('admin.transfer.index') }}">
+                {{ __('Transfer') }}
+            </a>
+        </li>
+
+        <li class="menu-item {{ isRoute('admin.expense.*', 'active') }}">
+            <a class="menu-link" href="{{ route('admin.expense.index') }}">
+                {{ __('Expenses') }}
+            </a>
+        </li>
+
+        <li class="menu-item {{ isRoute('admin.investor.*', 'active') }}">
+            <a class="menu-link" href="{{ route('admin.investor.index') }}">
+                {{ __('Investor') }}
+            </a>
+        </li>
+    </ul>
+</li>
+
+{{-- reports Income
+	Expenses
+--}}
+
+<li class="menu-item {{ isRoute(['admin.reports.income', 'admin.reports.expense'], 'active open') }}">
+    <a href="javascript:void(0);" class="menu-link menu-toggle">
+        <i class='menu-icon tf-icons bx bx-news'></i>
+        <div class="text-truncate" data-i18n="{{ __('Reports') }}">{{ __('Reports') }}</div>
+    </a>
+
+    <ul class="menu-sub">
+        <li class="menu-item {{ isRoute(['admin.reports.income'], 'active') }}">
+            <a class="menu-link" href="{{ route('admin.reports.income') }}">
+                {{ __('Income Report') }}
+            </a>
+        </li>
+        <li class="menu-item {{ isRoute(['admin.reports.expense'], 'active') }}">
+            <a class="menu-link" href="{{ route('admin.reports.expense') }}">
+                {{ __('Expense Report') }}
+            </a>
+        </li>
+    </ul>
+</li>
--- Modules/Accounting/routes/web.php
+++ Modules/Accounting/routes/web.php
@@ -1,8 +1,27 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use Modules\Accounting\Http\Controllers\AccountingController;
+use Modules\Accounting\app\Http\Controllers\AccountingController;
 
-Route::middleware(['auth', 'verified'])->group(function () {
-    Route::resource('accountings', AccountingController::class)->names('accounting');
+Route::group(['as' => 'admin.', 'prefix' => 'admin', 'middleware' => ['auth:admin', 'translation']], function () {
+    Route::resource('invoice', AccountingController::class)->names('invoice');
+    Route::resource('customer', AccountingController::class)->names('customer');
+    Route::resource('container', AccountingController::class)->names('container');
+    Route::resource('product', AccountingController::class)->names('product');
+    Route::resource('category', AccountingController::class)->names('category');
+
+    Route::resource('income', AccountingController::class)->names('income');
+    Route::resource('transfer', AccountingController::class)->names('transfer');
+    Route::resource('expense', AccountingController::class)->names('expense');
+    Route::resource('investor', AccountingController::class)->names('investor');
+
+    Route::get('reports/invoice', [AccountingController::class, 'invoiceReport'])->name('reports.invoice');
+    Route::get('reports/customer', [AccountingController::class, 'customerReport'])->name('reports.customer');
+    Route::get('reports/container', [AccountingController::class, 'containerReport'])->name('reports.container');
+    Route::get('reports/product', [AccountingController::class, 'productReport'])->name('reports.product');
+    Route::get('reports/category', [AccountingController::class, 'categoryReport'])->name('reports.category');
+    Route::get('reports/income', [AccountingController::class, 'incomeReport'])->name('reports.income');
+    Route::get('reports/transfer', [AccountingController::class, 'transferReport'])->name('reports.transfer');
+    Route::get('reports/expense', [AccountingController::class, 'expenseReport'])->name('reports.expense');
+    Route::get('reports/investor', [AccountingController::class, 'investorReport'])->name('reports.investor');
 });
